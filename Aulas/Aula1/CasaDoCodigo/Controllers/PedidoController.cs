89c61b5ed625bf2534b42f8f6c58cfbe9fcb344d
Felipe <felipepg@uniplaclages.edu.br>
2022-08-16 17:20:24 -0300

    Salva quantidade banco

--- Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
+++ Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -9,12 +9,15 @@ namespace CasaDoCodigo.Controllers
 
         private readonly IProdutoRepository produtoRepository;
         private readonly IPedidoRepository pedidoRepository;
+        private readonly IItemPedidoRepository itemPedidoRepository;
 
         public PedidoController(IProdutoRepository produtoRepository,
-            IPedidoRepository pedidoRepository)
+            IPedidoRepository pedidoRepository,
+            IItemPedidoRepository itemPedidoRepository)
         {
             this.produtoRepository = produtoRepository;
-            this.pedidoRepository  = pedidoRepository;
+            this.pedidoRepository = pedidoRepository;
+            this.itemPedidoRepository = itemPedidoRepository;
         }
 
         public IActionResult Carrossel()
@@ -42,6 +45,12 @@ namespace CasaDoCodigo.Controllers
         {
             Pedido pedido = pedidoRepository.GetPedido();
             return View(pedido);
+        }
+
+        [HttpPost]
+        public void UpdateQuantidade([FromBody]ItemPedido itemPedido)
+        {
+            itemPedidoRepository.UpdateQuantidade(itemPedido);
         }
     }
 }
--- Aulas/Aula1/CasaDoCodigo/Repositories/ItemPedidoRepository.cs
+++ Aulas/Aula1/CasaDoCodigo/Repositories/ItemPedidoRepository.cs
@@ -1,11 +1,30 @@
 ﻿using CasaDoCodigo.Models;
+using System.Linq;
 
 namespace CasaDoCodigo.Repositories
 {
-    public class ItemPedidoRepository : BaseRepository<ItemPedido>
+    public interface IItemPedidoRepository
     {
+        void UpdateQuantidade(ItemPedido itemPedido);
+    }
+    public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
+    {
+        
         public ItemPedidoRepository(ApplicationContext contexto) : base(contexto)
         {
+        }
+
+        public void UpdateQuantidade(ItemPedido itemPedido)
+        {
+            ItemPedido itemPedidoDB = dbSet.Where(ip => ip.Id == itemPedido.Id)
+                                           .SingleOrDefault();
+
+            if(itemPedidoDB != null)
+            {
+                itemPedidoDB.AtualizaQuantidade(itemPedido.Quantidade);
+
+                contexto.SaveChanges();
+            }
         }
     }
 }
