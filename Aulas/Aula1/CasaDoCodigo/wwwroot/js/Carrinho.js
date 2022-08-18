adf49e4064f8a064fdbede43be79ed07ebd584ca
Felipe <felipepg@uniplaclages.edu.br>
2022-08-18 20:18:27 -0300

    Atualiza quantidade carrinho

---
+++ Aulas/Aula1/CasaDoCodigo/wwwroot/js/Carrinho.js
@@ -0,0 +1,51 @@
+﻿class Carrinho
+{
+    clickIncremento(btn)
+    {
+        let data = this.GetData(btn);
+        data.Quantidade++;
+        this.PostQuantidade(data);
+    }
+
+    clickDecremento(btn)
+    {
+        let data = this.GetData(btn);
+        data.Quantidade--;
+        this.PostQuantidade(data);
+    }
+
+    updateQuantidade(input)
+    {
+        let data = this.GetData(input);        
+        this.PostQuantidade(data);
+    }
+
+    PostQuantidade(data) {
+        $.ajax({
+            url: '/pedido/updatequantidade',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(data)
+        });
+
+        $("input[name='quantidade']").val(data.Quantidade);
+    }
+
+    GetData(elemento) {
+        let linhaItem = $(elemento).parents('[item-id]');
+        let itemId = $(linhaItem).attr('item-id');
+        let novaQtde = $(linhaItem).find('input').val();
+
+        return {
+            Id: itemId,
+            Quantidade: novaQtde
+        };
+    }
+}
+
+var carrinho = new Carrinho();
+
+
+
+
+
